02d996fa7c5954a9dd3598bef705d4a540d7f0f1
FredLitt <80988264+FredLitt@users.noreply.github.com>
2022-07-12 13:44:30 -0700

    Added Todos file

--- chess_app_frontend/src/components/CreatedGameInfo.jsx
+++ chess_app_frontend/src/components/CreatedGameInfo.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function CreatedGameInfo({gameData, closePopUp}) {
 
+    const gameID = `${gameData.id}${gameData.color[0]}`
+
   return (
     <div id="created-game-info" unselectable='on'>
         <button className="close-popup-button" onClick={closePopUp}>X</button>
-        <div>Send the game ID to a friend for them to join: </div>
+        <div>Send the game ID to a friend for them to join</div>
         <div>
-            <div>{gameData.id} </div><button>Copy</button>
+            <div>{gameData.id} </div><button onClick={() =>  navigator.clipboard.writeText(gameID)}>Copy</button>
         </div>
         </div>
   )
--- chess_app_frontend/src/components/JoinGamePopUp.jsx
+++ chess_app_frontend/src/components/JoinGamePopUp.jsx
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 
 export default function JoinGamePopUp({joinGame, closePopUp}){
 
-const [ id, setID ] = useState(null)
+const [ id, setID ] = useState("")
 
 const handleKeydown = (e) => {
+  if (e.key === "Backspace"){
+    setID("")
+  }
+  if (e.key !== "Enter"){
+    setID(id)
+  }
   if (e.key === "Enter"){
     joinGame(id)
-    setID(null)
-  }
+    setID("")
+  } 
 }
 
 return (
   <>
     <div id="join-game-popup">Enter ID of game to join
       <button className="close-popup-button" onClick={closePopUp}>X</button>
-        <input type="text" onChange={(e) => setID(e.target.value)} onKeyDown={handleKeydown} />
+        <input type="text" value={id} onKeyDown={handleKeydown} onPaste={(e) => setID(e.clipboardData.getData("Text"))}/>
     </div>
   </>
 )}
